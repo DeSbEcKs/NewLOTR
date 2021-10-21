d87f396f36d3375d625e77e5014a02c51ee2e0ae
DeSbEcKs <92368428+DeSbEcKs@users.noreply.github.com>
2021-10-21 18:39:58 +0200

    Add files via upload

--- hdr_map_styles.js
+++ hdr_map_styles.js
@@ -11,87 +11,43 @@
 ;(function(a){a.fn.rwdImageMaps=function(){var c=this;var b=function(){c.each(function(){if(typeof(a(this).attr("usemap"))=="undefined"){return}var e=this,d=a(e);a("<img />").on('load',function(){var g="width",m="height",n=d.attr(g),j=d.attr(m);if(!n||!j){var o=new Image();o.src=d.attr("src");if(!n){n=o.width}if(!j){j=o.height}}var f=d.width()/100,k=d.height()/100,i=d.attr("usemap").replace("#",""),l="coords";a('map[name="'+i+'"]').find("area").each(function(){var r=a(this);if(!r.data(l)){r.data(l,r.attr(l))}var q=r.data(l).split(","),p=new Array(q.length);for(var h=0;h<p.length;++h){if(h%2===0){p[h]=parseInt(((q[h]/n)*100)*f)}else{p[h]=parseInt(((q[h]/j)*100)*k)}}r.attr(l,p.toString())})}).attr("src",d.attr("src"))})};a(window).resize(b).trigger("resize");return this}})(jQuery);
 
 
-/*Funktionsaufruf des obrigen Codes */
-$(document).ready(function(e) {
-			$('img[usemap]').rwdImageMaps();
-			
+$(document).ready(function(){
+  $("button").click(function(){
+    $("p").click();
+  });
 });
-				
-				
-/*Funktion um hidden div´s anzuzeigen*/
-				$('#Isengard').click(function(e){ 
-					e.preventDefault();
-					$(".hidden").hide();
-					$("#showIsengard").show()
+
+
+/*$("").click(function(){
+  $('').show();
+}); 
+
+/*
+$(document).ready(function(e) {
+				$('img[usemap]').rwdImageMaps();
 				});
+
+
+function myFunction() {
+					var ort;
+					var ortsDiv;
+					
+					
+					console.log(click.target.alt)
+					ort=click.target.alt;
+						console.log(ort)
+					ortsDiv=document.getElementById(ort);
+						console.log(ortsDiv)
+					ortsDiv.removeAttribute("class");
+					ortsDiv.setAttribute("class", "hidden"); 
+				//ortsDiv.classList.toggle('hidden');
 				
-				$('#Minas_Tirith').click(function(e){ 
-					e.preventDefault();
-					$(".hidden").hide();
-					$("#showMinas_Tirith").show()
-				});
-				
-				$('#Osgiliath').click(function(e){ 
-					e.preventDefault();
-					$(".hidden").hide();
-					$("#showOsgiliath").show()
-				});
-				
-				$('#Morannon').click(function(e){ 
-					e.preventDefault();
-					$(".hidden").hide();
-					$("#showMorannon").show()
-				});
-				
-				$('#Rivendell').click(function(e){ 
-					e.preventDefault();
-					$(".hidden").hide();
-					$("#showRivendell").show()
-				});
-				
-				$('#The_Shire').click(function(e){ 
-					e.preventDefault();
-					$(".hidden").hide();
-					$("#showThe_Shire").show()
-				});
-				
-				$('#Bree').click(function(e){ 
-					e.preventDefault();
-					$(".hidden").hide();
-					$("#showBree").show()
-				});
-				
-				$('#Laketown').click(function(e){ 
-					e.preventDefault();
-					$(".hidden").hide();
-					$("#showLaketown").show()
-				});
-				
-				$('#Edoras').click(function(e){ 
-					e.preventDefault();
-					$(".hidden").hide();
-					$("#showEdoras").show()
-				});
-				
-			
-	
-/*Tom's Menüband-Script*/			
-		const icon = document.querySelector('.icon');
-		const search = document.querySelector('.search')
-		const input = document.querySelector('#eingabe')
-		icon.onclick = function(){	
-		if (search.classList.contains ('active')){
-		
-		window.find(document.getElementById('eingabe').value)
-		search.classList.toggle('active')
-		
-		}else {search.classList.toggle('active')
-		input.focus();}
-		}
+					}	
+
+
 
 
 /*
-
 * rwdImageMaps jQuery plugin v1.6
 *
 * Allows image maps to be used in a responsive design by recalculating the area coordinates to match the actual image size on load and window.resize
